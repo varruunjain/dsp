ca7448ce3735e44e8f735c8426e6ff39b451b23d
varruunjain <varruunjain@gmail.com>
2017-12-20 01:10:22 -0800

    Added Advanced Python dictionary

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,97 @@
-PLACE YOUR CODE HERE
+facultycsv='''name, degree, title, email
+Scarlett L. Bellamy, Sc.D.,Associate Professor of Biostatistics,bellamys@mail.med.upenn.edu
+Warren B. Bilker,Ph.D.,Professor of Biostatistics,warren@upenn.edu
+Matthew W Bryan, PhD,Assistant Professor of Biostatistics,bryanma@upenn.edu
+Jinbo Chen, Ph.D.,Associate Professor of Biostatistics,jinboche@upenn.edu
+Susan S Ellenberg, Ph.D.,Professor of Biostatistics,sellenbe@upenn.edu
+Jonas H. Ellenberg, Ph.D.,Professor of Biostatistics,jellenbe@mail.med.upenn.edu
+Rui Feng, Ph.D,Assistant Professor of Biostatistics,ruifeng@upenn.edu
+Benjamin C. French, PhD,Associate Professor of Biostatistics,bcfrench@mail.med.upenn.edu
+Phyllis A. Gimotty, Ph.D,Professor of Biostatistics,pgimotty@upenn.edu
+Wensheng Guo, Ph.D,Professor of Biostatistics,wguo@mail.med.upenn.edu
+Yenchih Hsu, Ph.D.,Assistant Professor of Biostatistics,hsu9@mail.med.upenn.edu
+Rebecca A Hubbard, PhD,Associate Professor of Biostatistics,rhubb@mail.med.upenn.edu
+Wei-Ting Hwang, Ph.D.,Associate Professor of Biostatistics,whwang@mail.med.upenn.edu
+Marshall M. Joffe, MD MPH Ph.D,Professor of Biostatistics,mjoffe@mail.med.upenn.edu
+J. Richard Landis, B.S.Ed. M.S. Ph.D.,Professor of Biostatistics,jrlandis@mail.med.upenn.edu
+Yimei Li, Ph.D.,Assistant Professor of Biostatistics,liy3@email.chop.edu
+Mingyao Li, Ph.D.,Associate Professor of Biostatistics,mingyao@mail.med.upenn.edu
+Hongzhe Li, Ph.D,Professor of Biostatistics,hongzhe@upenn.edu
+A. Russell Localio, JD MA MPH MS PhD,Associate Professor of Biostatistics,rlocalio@upenn.edu
+Nandita Mitra, Ph.D.,Associate Professor of Biostatistics,nanditam@mail.med.upenn.edu
+Knashawn H. Morales, Sc.D.,Associate Professor of Biostatistics,knashawn@mail.med.upenn.edu
+Kathleen Joy Propert, Sc.D.,Professor of Biostatistics,propert@mail.med.upenn.edu
+Mary E. Putt, PhD ScD,Professor of Biostatistics,mputt@mail.med.upenn.edu
+Sarah Jane Ratcliffe, Ph.D.,Associate Professor of Biostatistics,sratclif@upenn.edu
+Michelle Elana Ross, PhD,Assistant Professor is Biostatistics,michross@upenn.edu
+Jason A. Roy, Ph.D.,Associate Professor of Biostatistics,jaroy@mail.med.upenn.edu
+Mary D. Sammel, Sc.D.,Professor of Biostatistics,msammel@cceb.med.upenn.edu
+Pamela Ann Shaw, PhD,Assistant Professor of Biostatistics,shawp@upenn.edu
+Russell Takeshi Shinohara,0,Assistant Professor of Biostatistics,rshi@mail.med.upenn.edu
+Haochang Shou, Ph.D.,Assistant Professor of Biostatistics,hshou@mail.med.upenn.edu
+Justine Shults, Ph.D.,Professor of Biostatistics,jshults@mail.med.upenn.edu
+Alisa Jane Stephens, Ph.D.,Assistant Professor of Biostatistics,alisaste@mail.med.upenn.edu
+Andrea Beth Troxel, ScD,Professor of Biostatistics,atroxel@mail.med.upenn.edu
+Rui Xiao, PhD,Assistant Professor of Biostatistics,rxiao@mail.med.upenn.edu
+Sharon Xiangwen Xie, Ph.D.,Associate Professor of Biostatistics,sxie@mail.med.upenn.edu
+Dawei Xie, PhD,Assistant Professor of Biostatistics,dxie@upenn.edu
+Wei (Peter) Yang, Ph.D.,Assistant Professor of Biostatistics,weiyang@mail.med.upenn.edu'''
+
+with open('faculty.csv','w') as f:
+    f.write(facultycsv)
+facultycsv.count("\n")
+
+def get_dict():
+    import csv
+    import re
+# Reading the file and storing it in respective list
+    with open("faculty.csv") as filename:
+        reader= csv.reader(filename, delimiter=',')
+        x_name=[]
+        x_degree=[]
+        x_title=[]
+        x_email=[]
+        for row in reader:
+            x_name.append(row[0])
+            x_degree.append(row[1])
+            x_title.append(row[2])
+            x_email.append(row[3])
+
+    
+    dicto=dict()
+    first_name=[]
+    last_name=[]
+    middle_name=[]
+    
+    for idx, w in enumerate(x_name[1:]):
+    
+        match=(re.search("([\w]+) ([\w+.]+) ([\w+]+)",w))
+        w=w.split(" ")
+    
+        first_name.append(w[0])
+    
+        last_name.append(w[len(w)-1])
+        if(len(w)>=3):
+            middle_name.append(w[1])
+        else:
+            middle_name.append(0)
+    
+    
+    for idx, l in enumerate(last_name):
+            
+            if(middle_name[idx]!=0):
+                
+                tup=(first_name[idx],middle_name[idx],last_name[idx])
+                dicto.update({tup:[x_degree[idx+1],x_title[idx+1],x_email[idx+1]]})
+            else:
+                tup=(first_name[idx],last_name[idx])
+        
+                dicto.update({tup:[x_degree[idx+1],x_title[idx+1],x_email[idx+1]]})
+                
+        
+      
+       
+            
+    return dicto
+
+
